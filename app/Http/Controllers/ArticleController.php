a3bb23aa1d7150711a69f30fcddf784265506d51
armin-drv <armindrv11@gmail.com>
2017-07-05 16:02:36 +0200

    fuck git

--- app/Http/Controllers/ArticleController.php
+++ app/Http/Controllers/ArticleController.php
@@ -11,6 +11,7 @@ class ArticleController extends Controller
 {
     public function getFirstTenArticles(){
 
+<<<<<<< HEAD
         $data = DB::table('articles')
             ->join('sections', 'articles.id', 'sections.article_id')
             ->where('sections.typeSection_id', 2)
@@ -21,5 +22,17 @@ class ArticleController extends Controller
             ->get();
 
         return response()->json($data);
+=======
+    	$data = DB::table('articles')
+    		->join('sections', 'articles.id', 'sections.article_id')
+    		->where('sections.typeSection_id', 2)
+    		->orderBy('articles.id', 'desc')
+    		->select('articles.id as id', 'articles.title as titre', 'articles.description', 'sections.content as imageUrl')
+    		->distinct()
+    		->limit(10)
+    		->get();
+
+    	return response()->json($data);
+>>>>>>> 78344a108e8f6e8508aa046e3f4be1138e020a3d
     }
 }
