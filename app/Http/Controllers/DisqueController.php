c153e696e55e9c8b9c50523d9a11d2101ab94f2f
ZacharieRudelle <zacharie.rudelle@gmail.com>
2017-07-08 22:08:46 +0200

    presque rien
    Retrait d'une ancienne fonction test

--- app/Http/Controllers/DisqueController.php
+++ app/Http/Controllers/DisqueController.php
@@ -47,40 +47,4 @@ class DisqueController extends Controller
 	    return response()->json($data);
 
     }  
-
-    public function index() {
-
-    	$data = DB::table('disques')
-    		->join('disque_titres', 'disques.id', 'disque_titres.disque_id')
-	     	->join('titres', 'titres.id', 'disque_titres.titre_id')
-	     	->select('titres.track_title', 'disques.disque_title', 'disque_titres.track_number')
-	     	->get();
-
-	    $disqueIDs = DB::table('disques')->select('id')->get();
-
-	    $disqueIDs = $disqueIDs->toArray();
-	   
-	    for($i = 0;$i < sizeof($disqueIDs); $i++){
-	    	$dataa[$i] = DB::select(DB::raw('SELECT `titres`.`track_title` 
-				FROM disques
-				INNER JOIN `disque_titres` ON `disques`.`id` = `disque_titres`.`disque_id` 
-				INNER JOIN `titres` ON `titres`.`id` = `disque_titres`.`titre_id`
-				WHERE (`disque_titres`.`disque_id` = '.$disqueIDs[$i]->id.' AND `titres`.`id` = `disque_titres`.`titre_id`)'));
-	    }		
-
-	    $trackTitle = DB::table('disques')
-	    	->join('disque_titres', 'disques.id', 'disque_titres.disque_id')
-	     	->join('titres', 'titres.id', 'disque_titres.titre_id')
-	     	->where([
-	     		['disques.id', '=', 'disque_titre.disque_id'],
-	     		['disque_titres.titre_id', '=', 'titres.id']
-	     	])
-	     	->select('titres.track_title') 
-	     	->get();
-
-	     	$dataaCol = collect($dataa);
-	     	echo(gettype($dataaCol));
-	     	//dd($dataaCol);
-    	return view('disques.index', ['name' => $data, 'title' => $data]);
-    }
 }
