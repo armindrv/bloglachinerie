001b4be7c2971ccec841abcecdda1388f0534672
ZacharieRudelle <zacharie.rudelle@gmail.com>
2017-08-31 13:26:01 +0200

    ajout des artistes à la fonction getDisquesFromLabel

--- app/Http/Controllers/DisqueController.php
+++ app/Http/Controllers/DisqueController.php
@@ -47,4 +47,38 @@ class DisqueController extends Controller
 	    return response()->json($data);
 
     }  
+
+    // Retourne tous les disques d'un label
+    public function getDisqueFromLabel($id) {
+        // Récupère l'id, le titre et l'image des disques du label
+        $data = DB::table('disques')
+            ->where('label_id', $id)
+            ->select('disques.id', 'disques.disque_title', 'disques.image_url')
+            ->get();
+
+        // Récupère le ou les artistes présents sur les disques
+        foreach($data as $disque) {
+           
+        $res = DB::table('disques')
+            ->join('disque_titres', 'disques.id', 'disque_titres.disque_id')
+            ->join('titres', 'titres.id', 'disque_titres.titre_id')
+            ->join('titre_artistes', 'titres.id', 'titre_artistes.titre_id')
+            ->join('artistes', 'titre_artistes.artiste_id', 'artistes.id')
+            ->where('disques.id', $disque->id)
+            ->groupBy('artistes.name')
+            ->select('artistes.name')
+            ->get();
+            
+            // ajout des artistes au disque
+            $array[] = array("id"=>$disque->id, "disque_title"=>$disque->disque_title, "image_url"=>$disque->image_url, "artiste_name"=>$res);
+
+            
+        }
+        if(isset($array)){
+            return response()->json($array);
+        } else {
+            return null;
+        }
+        
+    }
 }
